d228a0e9b4d701607fa49ffed75a901c1d803d91
Ela <64909186+Ela17@users.noreply.github.com>
2022-10-24 10:11:41 +0200

    Modificato Ex1

--- Programmazione 1 Lab/Esempi lezione/Ex1 (con spiegazione passo-passo).java
+++ Programmazione 1 Lab/Esempi lezione/Ex1 (con spiegazione passo-passo).java
@@ -10,7 +10,9 @@ class Ex1 {
     }
 }
 /* SPIEGAZIONE
+
 Calcolo F(n):   se n == 0 allora 0, altrimenti F(n - 1) + 1
+
 prendiamo ad esempio n = 4:
 F(4) = F(3) + 1 // prendo il secondo caso perchè n!=0
 quanto vale F(3) ?
@@ -21,6 +23,7 @@ quanto vale F(1) ?
 F(1) = F(0) + 1 
 quanto vale F(0) ?
 F(0) == 0 !!!
+
 Quindi so quanto vale F(1):
     infatti F(1) = 0 + 1 = 1
 Ora posso calcolare F(2)
@@ -29,4 +32,5 @@ Quindi conosco F(3):
     infatti F(3) = F(2) + 1 = (F(1)+ 1) + 1 = 2 + 1 = 3
 Infine ottengo F(4):
     infatti F(4) = F(3) + 1 = (F(2)+ 1) + 1 = F(F(F(1)) + 1) + 1 = 3 + 1 = 4
+    
 */
