12d7d74ff8f915189cfed90ce7a1b00e2b50da87
Ela <64909186+Ela17@users.noreply.github.com>
2022-11-07 09:40:45 +0100

    Update Array.java

--- Programmazione 1/Esempi lezione (Cardone)/Array.java
+++ Programmazione 1/Esempi lezione (Cardone)/Array.java
@@ -22,12 +22,40 @@ public class Array {
         return b;
     }
 
-    public static boolean uguali_var(int[] x, int[] y){
+    public static boolean uguali_var(int[] x, int[] y) { 
+        // algoritmo dicotomico: suddivido il problema in due sottoproblemi e li risolvo separatamente
         int i = -1;
         if(x.length == y.length)
             for(i = 0; i < x.length && x[i] == y[i]; i++);
         
         return i == x.length;
+    }
+
+    static void max(int[] a) {
+        // porta l'elemento massimo di a in ultima posizione (a.length - 1)
+        // confrontando ogni elemento col precedente
+        for(int i = 1; i <= a.length - 1; i++)  // parto da i = 1 perchè a[0] non ha precedente
+            if (a[i-1] > a[i]) {    // se il precedente è maggiore, li scambio.
+                int x = a[i];
+                a[i] = a[i-1];
+                a[i-1] = x;
+            }
+    }
+
+    static void max_rel(int[] a, int p) {   // p è una posizione arbitraria in mezzo al vettore a
+        // considero solo il sottovettore che va da 0 a p
+        // a.length > 1, p < a.length
+        for(int i = 1; i <= p; i++)
+            if (a[i-1] > a[i]) {
+                int x = a[i];
+                a[i] = a[i-1];
+                a[i-1] = x;
+            }
+    }
+
+    static void ordina (int[] a) {  
+        for(int i = a.length-1; i > 0; i--)
+            max_rel(a, i);  // ad ogni chiamata considero il sottovettore sempre più corto.
     }
 
     public static void main(String[] args) {
