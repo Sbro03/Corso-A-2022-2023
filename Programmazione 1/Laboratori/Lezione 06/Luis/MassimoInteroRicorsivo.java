ba857592ddbbccde61c2d2715065d0c660f80ed0
marialuisabarrios <luisbarriosaraujo@outlook.it>
2022-11-08 16:26:55 +0100

    Modifiche dei commenti ad alcuni es

--- Programmazione 1/Laboratori/Lezione 06/Luis/MassimoInteroRicorsivo.java
+++ Programmazione 1/Laboratori/Lezione 06/Luis/MassimoInteroRicorsivo.java
@@ -5,17 +5,20 @@ public class MassimoInteroRicorsivo{
 	}
 	public static int MaxMin(){
 		Scanner input = new Scanner(System.in);
-		System.out.println("Inserisci il numero :");
-		int m= input.nextInt();
+		System.out.println("Inserisci il numero :");//input della stringa
+		int m= input.nextInt();// digito in tastiera della stringa desiderata
 		if(m==0){
-			return 0;
+			/*se la stringa è uguale a 0*/
+			return 0;//ritorna 0
 		}
-		int function = MaxMin();
+		int function = MaxMin();//variabile per dire che per fare il confronto delle variabili
 		if(function<m){
-			return m;
+			/*se m è maggiore di function allora*/
+			return m;//m è maggiore
 		}
 		else{
-			return function;
+			/*altrimenti*/
+			return function;//la funzione è maggiore
 		}
 	}
 }
